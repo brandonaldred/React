459a39ae7061753b4f658327ee546ac01efc6f85
Brandon Aldred <brandonaldred@gmail.com>
2023-04-26 11:13:16 -0700

    Adding dictionary project

---
+++ FEM-dictionary-web-app/client/src/App.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+import { ThemeProvider } from 'styled-components'
+import tokens from './assets/design-tokens.tokens.json'
+import { useState } from 'react'
+import logo from './assets/iconoir_book.svg'
+import modeIcon from './assets/iconoir_half-moon.svg'
+import './App.css'
+
+// theme selection is based on user input - dark, light//
+// font options are based on user input - serif, sans, mono //
+// font size options are based on screen size - s, m, l //
+
+
+function App() {
+  const [ theme, setTheme ] = useState('light')
+  const isDark = theme === 'dark' ? true : false
+  
+
+  function toggleMode(bool) {
+    setTheme(bool ? 'light' : 'dark')
+  }
+
+  document.documentElement.dataset.theme = theme;
+
+  return (
+      <header>
+        <img class="logo" src={logo} alt="Dictionary Web App" />
+          <select class="font-select">
+            <option>Sans</option>
+            <option>Serif</option>
+            <option>Mono</option>
+          </select>
+          <label>
+            <span></span>
+            <input type="checkbox" onChange={() => { toggleMode(isDark) }} />
+            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" clipRule="evenodd" d="M1 10.449C0.998458 12.8283 1.80169 15.1383 3.27914 17.0033C4.75659 18.8683 6.82139 20.1788 9.13799 20.7218C11.4545 21.2647 13.8866 21.0082 16.039 19.994C18.1912 18.9797 19.9373 17.2673 20.9931 15.1352C11.5442 15.1352 6.85799 10.4479 6.85799 1C5.09842 1.87311 3.61767 3.22033 2.58266 4.88981C1.54765 6.5593 0.999502 8.48469 1 10.449Z" stroke="currentcolor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+          </label> 
+      </header>
+  )
+}
+
+export default App
