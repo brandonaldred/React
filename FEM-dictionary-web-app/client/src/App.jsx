56a132e1e6ae2d1a5bb02e760948e249e77609a0
Brandon Aldred <brandonaldred@gmail.com>
2023-04-27 07:22:20 -0700

    adding in API info

--- FEM-dictionary-web-app/client/src/App.jsx
+++ FEM-dictionary-web-app/client/src/App.jsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 import { ThemeProvider } from 'styled-components'
-import { useState } from 'react'
-import Header from './assets/components/header'
+import { useState, useEffect } from 'react'
+import Header from './assets/components/Header'
+import Definition from './assets/components/Definition'
 import search from './assets/iconoir_search.svg'
 import playButton from './assets/play-button.svg'
 import './App.css'
@@ -25,45 +26,30 @@ function App() {
   document.documentElement.dataset.font = font
 
   const [word, setWord] = useState('')
+  const [definition, setDefinition] = useState([])
 
-  function runSearch(e) {
-    //perform search
-    console.log(e)
-    //empty searchbar
-    setWord('');
+  function createComponent (json) {
+    useEffect(()=> {
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${w}`)
+        .then(res => res.json())
+        .then(json => setDefinition(json))
+        .catch(err => console.log(err)) 
+      }, [definition])
+
   }
+
+    
 
   return (
       <>
         <Header isDark={isDark} toggleMode={toggleMode} font={font} />
         <label id="search">
           <input value={word} type="text" onChange={(e)=> {setWord(e.target.value)}} onSubmit={(e) => {runSearch(e)}} />
-          <img src={search}  onClick={()=>{runSearch(word)}}/>
+          <img src={search}  onClick={()=>{createComponent(word)}}/>
+          {definition.length > 0 && <Definition definition={definition}/>}
         </label>
-        <main>
-          <h2>word</h2>
-          <p>pronunciation</p>
-          <img src={playButton} />
-          <h3>type</h3>
-          <h4>Meaning</h4>
-          <ul>
-            <li>meaning 1</li>
-            <li>meaning 2</li>
-            <li>meaning 3</li>
-          </ul>
-          <h4>Synonyms <span>word synonym</span></h4>
-          <h3>type</h3>
-          <ul>
-            <li>meaning 1</li>
-            <li>meaning 2</li>
-            <li>meaning 3</li>
-          </ul>
-          <hr />
-          <p>Source <span>www.sourceURL</span></p>
-        </main>
-      </>
         
-
+      </> 
   )
 }
 
